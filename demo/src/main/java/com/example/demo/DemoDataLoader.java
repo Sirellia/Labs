7391517fbb5dbe768c85962b0931c141e9553f5f
Sirellia <124346074+Sirellia@users.noreply.github.com>
2025-07-19 16:57:35 +0400

    Update DemoDataLoader.java

--- demo/src/main/java/com/example/demo/DemoDataLoader.java
+++ demo/src/main/java/com/example/demo/DemoDataLoader.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 import com.example.demo.dto.RestaurantRequestDTO;
+import com.example.demo.dto.RestaurantResponseDTO;
 import com.example.demo.dto.ReviewRequestDTO;
 import com.example.demo.dto.VisitorRequestDTO;
+import com.example.demo.dto.VisitorResponseDTO;
 import com.example.demo.model.CuisineType;
 import com.example.demo.model.Visitor;
 import com.example.demo.service.RestaurantService;
@@ -11,6 +13,8 @@ import com.example.demo.service.VisitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class DemoDataLoader implements CommandLineRunner {
@@ -27,18 +31,18 @@ public class DemoDataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        visitorService.save(new VisitorRequestDTO("Иван", 25, Visitor.Gender.MALE));
-        visitorService.save(new VisitorRequestDTO(null, 30, Visitor.Gender.FEMALE));
-        visitorService.save(new VisitorRequestDTO("Анна", 22, Visitor.Gender.FEMALE));
+        VisitorResponseDTO v1 = visitorService.save(new VisitorRequestDTO("Иван", 25, Visitor.Gender.MALE));
+        VisitorResponseDTO v2 = visitorService.save(new VisitorRequestDTO(null, 30, Visitor.Gender.FEMALE));
+        VisitorResponseDTO v3 = visitorService.save(new VisitorRequestDTO("Анна", 22, Visitor.Gender.FEMALE));
 
-        restaurantService.save(new RestaurantRequestDTO("Пиццерия Италия", "Настоящая итальянская пицца", CuisineType.ITALIAN, 800));
-        restaurantService.save(new RestaurantRequestDTO("Дракон", "Лучший китайский ресторан", CuisineType.CHINESE, 600));
-        restaurantService.save(new RestaurantRequestDTO("Европа", "", CuisineType.EUROPEAN, 1000));
+        RestaurantResponseDTO r1 = restaurantService.save(new RestaurantRequestDTO("Пиццерия Италия", "Настоящая итальянская пицца", CuisineType.ITALIAN, 800));
+        RestaurantResponseDTO r2 = restaurantService.save(new RestaurantRequestDTO("Дракон", "Лучший китайский ресторан", CuisineType.CHINESE, 600));
+        RestaurantResponseDTO r3 = restaurantService.save(new RestaurantRequestDTO("Европа", "", CuisineType.EUROPEAN, 1000));
 
-        reviewService.save(new ReviewRequestDTO(1L, 1L, 5, "Очень вкусно!"));
-        reviewService.save(new ReviewRequestDTO(2L, 1L, 4, "Пицца хорошая, но долго ждал"));
-        reviewService.save(new ReviewRequestDTO(3L, 2L, 3, "Средне"));
-        reviewService.save(new ReviewRequestDTO(1L, 2L, 4, "Вкусная лапша!"));
-        reviewService.save(new ReviewRequestDTO(3L, 3L, 5, "Отлично!"));
+        reviewService.save(new ReviewRequestDTO(v1.id(), r1.id(), 5, "Очень вкусно!"));
+        reviewService.save(new ReviewRequestDTO(v2.id(), r1.id(), 4, "Пицца хорошая, но долго ждал"));
+        reviewService.save(new ReviewRequestDTO(v3.id(), r2.id(), 3, "Средне"));
+        reviewService.save(new ReviewRequestDTO(v1.id(), r2.id(), 4, "Вкусная лапша!"));
+        reviewService.save(new ReviewRequestDTO(v3.id(), r3.id(), 5, "Отлично!"));
     }
 } 
